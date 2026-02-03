014d1b69119eb23d47f7c2bb5666b90cde953400
AnaKonjaria <anikokonjaria.ak@gmail.com>
2026-02-03 15:43:54 +0400

    Level 1

---
+++ Level 1/Homework/HW1.py
@@ -0,0 +1,30 @@
+#gender options variables
+G1='Female'
+G2='Male' 
+
+name='Ana'
+surname='Konjaria'
+age=28
+gender=G1
+status='married'
+
+print(name, surname, age, 'years old', gender, 'is', status )
+
+#created 5 variables and printed them in order to see the outcome.
+
+
+print(5+5)
+print(10/3)
+print(10//3)
+print(5*5)
+print(5-3)
+
+#user 5 types of mathematical operations
+
+print(type(name))
+print(type(age))
+print(type(gender))
+print(type(status))
+print(type(surname))
+
+#On the first lesson we studied print function; date types: Integer, String, Float; created variables; five types of mathematical operations and how to discover which type of date we have used using print(type()) function. 
