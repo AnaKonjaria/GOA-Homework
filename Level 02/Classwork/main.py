297129daf97f87c4dc0d9450276e6676a0f98673
AnaKonjaria <anikokonjaria.ak@gmail.com>
2026-02-07 13:45:12 +0400

    first commit

---
+++ Level 02/Classwork/main.py
@@ -0,0 +1,7 @@
+#how many seconds are in a day?
+
+hours=24
+minutes=60
+seconds=60
+
+print(hours * minutes * seconds)
---
+++ Level 02/Homework/main.py
@@ -0,0 +1,25 @@
+#Code is executed from the top to bottom, line by line. 
+#We have to create variable before calling it, so first line will be declaring a variable, the next line will be print() function.
+#Debugging consists of two steps: identifying bug and fixing it. Examples:
+
+#
+
+#Here are fixed bugs examples:
+
+print('hello world')
+name = 'Ana'
+name2 = 'Anna'
+age = 28
+surname = 'konjaria'
+print(surname)
+
+#building a sentence using variables:
+print(name, surname, 'is', age, 'years old.')
+
+#repeating numerical data:
+
+print(5+5)
+print(3*5)
+print(5/2)
+print(5//2)
+print(3-1)
