8626318282786ed324d7dff9f2c338eafed02682
AnaKonjaria <anikokonjaria.ak@gmail.com>
2026-02-10 20:41:45 +0400

    commit

---
+++ Level 04/Classwork/CW.py
@@ -0,0 +1,12 @@
+print(23>11)
+print(10<13)
+print(10!=10)
+print(10!=11)
+print(10<=10)
+print(10>=12)
+
+print(11%3)
+print(3**4)
+
+print(False or True)
+print(True and False)
---
+++ Level 04/Homework/HW.py
@@ -0,0 +1,79 @@
+#1age mirror
+
+# age=int(input("How old are you?: "))
+# print(age>18)
+# print(type(age))
+
+
+#2two numbers comparison
+# num1=float(input("Choose a number: "))
+# num2=float(input("Choose one more number: "))
+# addition=(num1 +num2)
+# multiplication=(num1*num2)
+# more_than=(addition>multiplication)
+# print(str(addition)+" is more than "+str(multiplication)+": "+str(more_than)) 
+
+# print(type(addition))
+# print(type(multiplication))
+
+#3truth game
+
+# numx=int(input("Choose a number: "))
+# numy=int(input("Choose another number: "))
+
+# print(numx>numy)
+# print(numx>=0)
+# print(numy>=0)
+
+
+#4 name and age contract
+name=input("What's your name?: ")
+age=int(input("How old are you?: "))
+age_validation=(age>=21)
+print("User "+name+" is adult: "+str(age_validation))
+
+#5 Calculator which thinks
+# number1=int(input())
+# number2=int(input())
+# division=(number1/number2)
+# is_float=(type(division)==float)
+# print(str(division)+" is float:"+str(is_float))
+
+
+#6 height task
+# height=float(input("How tall are you?: "))
+# height_meters=(height/100)
+# print((str(height_meters))+" is more than 1.75 meters: "+str((height_meters>1.75)))
+
+#7number's character
+
+# numberx=int(input("Enter the number: "))
+# print("is positive: "+str(numberx>=0))
+# print("is negative: "+str(numberx<=0))
+# print("is equal to zero: "+str(numberx==0))
+
+#8 String vs number
+
+# numberY=input("Enter any number: ")
+# numberZ=int(numberY)
+# print(type(numberZ))
+# print("Number is more than 100: "+str(numberZ>100))
+
+#9 average is not just a number
+
+# Float1=float(input())
+# Float2=float(input())
+# Float3=float(input())
+# average=((Float1+Float2+Float3)/3)
+# print(type(average>50))
+# print("Average is more than 50: "+ str(average>50))
+
+#10 Detective of types
+
+# char=input("Enter anything: ")
+# char1=int(char)
+# char2=float(char)
+# print(type(char1))
+# print(type(char2))
+# print(char1==float)
+# print(char2==float)
