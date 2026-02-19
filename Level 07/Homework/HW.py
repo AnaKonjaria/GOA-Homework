365bb06571f98603496f168a01553fb94593629b
AnaKonjaria <anikokonjaria.ak@gmail.com>
2026-02-19 16:13:15 +0400

    commit

---
+++ Level 07/Homework/HW.py
@@ -0,0 +1,86 @@
+#2
+
+# sum=0
+# for i in range (1, 5):
+#    print(i)
+#    sum +=i
+# print(sum)
+
+
+#3
+
+# password="1234"
+# user_input=input("Enter the password: ")
+# while user_input!=password:
+#    user_input=input("Try again: ")
+# print("Access granted")
+
+#4
+
+# n=int(input("Enter any number: "))
+
+# for i in range (1, n):
+#     print(i)
+
+#მეოთხე დავალებას while ციკლით მგონი ვერ გავაკეთებთ. დავალებაში მოთხოვნილია while ციკლი.
+
+# 5
+
+# total=0
+# count=0
+# num_inputs=3
+# while count<num_inputs:
+#     n1=int(input())
+#     total +=n1
+#     count +=1
+# average=(total/count)
+# print(average)
+
+
+# #8
+# count=1
+# user_input=3
+
+# while count<=user_input:
+#     num1=int(input("Write any number: "))
+#     print(type(num1), count)
+#     count +=1
+
+
+#9
+
+# numX=int(input("First num: "))
+# numY=int(input("Second num: "))
+# print(numX>5 and numY<10)
+
+#10
+
+# multiplication=1
+# for i in range(1, 5):
+#     multiplication *= i
+# print(multiplication)
+
+#11
+
+
+# for i in range(1, 5):
+#     square=i**2
+#     print(square)
+
+
+#12
+
+# count=0
+
+# while count<10:
+#     print("hello")
+#     count+=1
+
+#13
+
+sum=0
+
+while sum<50:
+    numZ=int(input("enter number: "))
+    sum+=numZ
+print(sum)
---
+++ Level 08/Classwork/CW.py
@@ -0,0 +1,28 @@
+# count=1
+
+# while count <=5:
+#     if count ==3:
+#         print("Three found!")
+#     else:
+#         print("Number", count)
+#     count +=1
+
+
+
+# num=5
+
+# while num>0:
+#     if num ==1:
+#         print("last number")
+#     else:
+#         print("Number:", num)
+#     num-=1
+
+
+temp=19
+if temp ==22:
+    print("Optimal")
+    if temp<22:
+        print("less then optimal")
+else:
+    print("above optimal")
