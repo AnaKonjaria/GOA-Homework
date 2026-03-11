625709524f8f7be5076913170486ea8932612cac
AnaKonjaria <anikokonjaria.ak@gmail.com>
2026-03-11 13:39:13 +0400

    commit

---
+++ Level 15/Homework/HW.py
@@ -0,0 +1,54 @@
+#1
+
+# a = "code"
+# b = "wa.rs"
+# name = a + b
+
+#2
+
+# def is_even(n): 
+#     if n % 2 == 0:
+#         return True
+#     else:
+#         return False
+
+#3
+
+# def main (verb, noun):
+#     return verb + noun
+
+#4
+
+# def even_or_odd(number):
+#     if number%2==0:
+#         return "Even"
+#     else:
+#         return "Odd"
+
+#5
+
+# def number_to_string(num):
+#     return (str(num))
+
+#6
+
+# def boolean_to_string(b):
+#     return (str(b))
+
+#7
+
+# def say_hello(name):
+    
+#     return f"Hello, {name}"
+
+#8
+
+# def quarter_of(month):
+#     if month >0 and month <4:
+#         return 1
+#     elif month >3 and month <7:
+#         return 2
+#     elif month >6 and month <10:
+#         return 3
+#     else:
+#         return 4
