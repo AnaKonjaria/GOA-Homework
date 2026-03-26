d67aa2a26bae0a1bffe42941eeac4c780bd22fc2
AnaKonjaria <anikokonjaria.ak@gmail.com>
2026-03-26 17:15:23 +0400

    commit

---
+++ Level 22/Homework/HW.py
@@ -0,0 +1,39 @@
+# Codewars
+# 1
+# def count_positives_sum_negatives(arr):
+#     count=0
+#     sum_negative=0
+#     if not arr:
+#         return[]
+#     for num in arr:
+#         if num >0:
+#             count+=1
+#         else:
+#             sum_negative+=num
+    
+#     return[count, sum_negative]
+            
+
+# 2
+# def litres(time):
+#     litres=int(time*0.5)
+#     return litres
+
+# 3
+# def monkey_count(n):
+#     array=[]
+#     for i in range(1, n+1):
+#         array.append(i)
+#     return array
+        
+# 4
+# def double_integer(i):
+#     return i*2
+
+# 5
+# def hero(bullets, dragons):
+#     return (bullets>=dragons*2)
+
+# 6
+# def is_even(n): 
+#     return n%2==0
